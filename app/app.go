e1700aa8a7b5c19ec825d28b8999f62aea7f8f52
Morvana Bonin <canyslupus@gmail.com>
2021-10-22 16:54:00 -0300

    Test funcional

--- app/app.go
+++ app/app.go
@@ -1,1 +1,15 @@
 package app
+
+import "net/http"
+
+// add = post
+// update = put
+// list = get
+
+func ListHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Pokemon!\n"))
+	
+	if err != nil {
+		return
+	}
+}
--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
 	"github.com/gorilla/mux"
+	"github.com/morvanabonin/golang_api/app"
+	"net/http"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello World API")
 
-	route := mux.NewRouter()
-	route.HandleFunc("/app", PokemonHandler)
-	http.Handle("/", route)
+	apiRoute := mux.NewRouter()
+	apiRoute.HandleFunc("/pokemons/", app.ListHandler).
+		Methods("GET")
+
+	srv := &http.Server{
+		Handler:      apiRoute,
+		Addr:         "127.0.0.1:5000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		return 
+	}
 }
